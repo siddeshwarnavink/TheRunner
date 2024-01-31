4261336b8964e9d0058e3c013ea26ee8442c3ff9
Siddeshwar <siddeshwar.work@gmail.com>
2024-01-31 19:57:12 +0530

    loading mc from sprite

--- src/Actor/MainCharacter.cpp
+++ src/Actor/MainCharacter.cpp
@@ -1,63 +1,81 @@
 #include "Actor/MainCharacter.h"
 
-namespace Actor {
+namespace Actor
+{
     sf::Sprite charactersprite;
 
-    MainCharacter::MainCharacter() : state(PlayerState::FALLING), jumpHeight(130.0f) {
-        if (!texture.loadFromFile("./assets/Character/mc.png")) {
+    MainCharacter::MainCharacter() : state(PlayerState::FALLING), jumpHeight(130.0f), spriteRegion(228, 36, 24, 24)
+    {
+        if (!texture.loadFromFile("./assets/texture/entity/main-character.png"))
+        {
             return;
         }
         charactersprite.setTexture(texture);
-        charactersprite.setPosition(40.0f, 200.0f);
+        charactersprite.setPosition(10.0f, 10.0f);
+        charactersprite.setTextureRect(spriteRegion);
+        charactersprite.setScale(3.5f, 3.5f);
         initialPosition = charactersprite.getPosition();
     }
 
-    void MainCharacter::display(sf::RenderWindow& window) {
+    void MainCharacter::display(sf::RenderWindow &window)
+    {
         window.draw(charactersprite);
     }
 
-    void MainCharacter::loop() {
-        if(state != PlayerState::DEAD) {
+    void MainCharacter::loop()
+    {
+        if (state != PlayerState::DEAD)
+        {
             // Jump logic
-            if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && state == PlayerState::RUNNING)
-                || state == PlayerState::JUMPING) {
-                
-                if(charactersprite.getPosition().y >= jumpHeight) {
+            if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && state == PlayerState::RUNNING) || state == PlayerState::JUMPING)
+            {
+
+                if (charactersprite.getPosition().y >= jumpHeight)
+                {
                     state = PlayerState::JUMPING;
                     charactersprite.move(0.0f, -0.15f);
-                } else {
+                }
+                else
+                {
                     state = PlayerState::FALLING;
                 }
             }
-            else {
+            else
+            {
                 initialPosition = charactersprite.getPosition();
                 charactersprite.move(0.0f, 0.0f);
             }
         }
 
         // Void kill
-        if(charactersprite.getPosition().y >= 500.f) {
+        if (charactersprite.getPosition().y >= 500.f)
+        {
             state = PlayerState::DEAD;
         }
     }
 
-    void MainCharacter::move(float x, float y) {
+    void MainCharacter::move(float x, float y)
+    {
         charactersprite.move(x, y);
     }
 
-    sf::FloatRect MainCharacter::getGlobalBounds() {
+    sf::FloatRect MainCharacter::getGlobalBounds()
+    {
         return charactersprite.getGlobalBounds();
     }
 
-    sf::Vector2f MainCharacter::getPosition() {
+    sf::Vector2f MainCharacter::getPosition()
+    {
         return charactersprite.getPosition();
     }
 
-    void MainCharacter::setState(PlayerState state) {
+    void MainCharacter::setState(PlayerState state)
+    {
         this->state = state;
     }
 
-    PlayerState MainCharacter::getState() {
+    PlayerState MainCharacter::getState()
+    {
         return this->state;
     }
 }
--- src/Actor/MainCharacter.h
+++ src/Actor/MainCharacter.h
@@ -2,30 +2,34 @@
 
 #include <SFML/Graphics.hpp>
 
-namespace Actor {
-    enum PlayerState {
+namespace Actor
+{
+    enum PlayerState
+    {
         RUNNING,
         DEAD,
         JUMPING,
         FALLING
     };
 
-    class MainCharacter {
-        private:
-            sf::Texture texture;
-            sf::Sprite charactersprite;
-            PlayerState state;
-            float jumpHeight;
+    class MainCharacter
+    {
+    private:
+        sf::Texture texture;
+        sf::IntRect spriteRegion;
+        sf::Sprite charactersprite;
+        PlayerState state;
+        float jumpHeight;
 
-        public:
-            sf::Vector2f initialPosition;
-            MainCharacter();
-            void loop();
-            void display(sf::RenderWindow& window);
-            void move(float x, float y);
-            sf::Vector2f getPosition();
-            sf::FloatRect getGlobalBounds();
-            void setState(PlayerState state);
-            PlayerState getState();
+    public:
+        sf::Vector2f initialPosition;
+        MainCharacter();
+        void loop();
+        void display(sf::RenderWindow &window);
+        void move(float x, float y);
+        sf::Vector2f getPosition();
+        sf::FloatRect getGlobalBounds();
+        void setState(PlayerState state);
+        PlayerState getState();
     };
 }
